1268d1c43b26bb573e9bef2d80411dcf52a27b0a
Jun Woo <wooh@mskcc.org>
2020-05-01 12:43:02 -0400

    maf2cat2

---
+++ exec/maf2cat2.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+# Convert maf file with Ref_Tri column into catalog
+library('argparse')
+library('tempoSig')
+
+parser <- ArgumentParser(description = 'Construct mutational catalog from MAF file with Ref_Tri column')
+parser$add_argument('maf', metavar = 'MAF',
+                    help = 'input MAF file')
+parser$add_argument('catalog', metavar = 'CATALOG', 
+                    help = 'output catalog file')
+args <- parser$parse_args()
+
+catalog <- maf2cat(maf = args$maf)
+write.table(catalog, file = args$catalog, quote = F, sep = '\t')
